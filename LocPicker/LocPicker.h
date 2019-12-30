7daf2d1e5cf8cffc473f4d6be0340b817f1d6a36
Anup Sukumaran <anup.sukumaran9@gmail.com>
2019-12-30 17:29:56 +0530

    Initial Commit

---
+++ LocPicker/LocPicker.h
@@ -0,0 +1,19 @@
+//
+//  LocPicker.h
+//  LocPicker
+//
+//  Created by Manu Puthoor on 30/12/19.
+//  Copyright © 2019 Manu Puthoor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocPicker.
+FOUNDATION_EXPORT double LocPickerVersionNumber;
+
+//! Project version string for LocPicker.
+FOUNDATION_EXPORT const unsigned char LocPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocPicker/PublicHeader.h>
+
+
